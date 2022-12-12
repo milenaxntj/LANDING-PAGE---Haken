e7fb21d182892e19c6f46cb7ed902d1d13dfc79c
milenaxntj <michurata@gmail.com>
2022-12-11 21:57:34 -0300

    Finalizando responsividade

--- script.js
+++
@@ -1,6 +0,0 @@
-const iconesDarkMode = document.getElementById(".iconesDarkMode");
-
-iconesDarkMode.addEventListener("change", (e) =>{
-    document.body.classList.toggle("dark", e.target.checked);
-})
-
